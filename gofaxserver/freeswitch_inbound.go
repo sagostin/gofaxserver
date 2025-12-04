dc9213b07b1faf4be1c145ecd3ab5e5b118e326e
Shaun Agostinho <shaunagostinho@gmail.com>
2025-12-03 18:17:14 -0800

    huh?

--- gofaxserver/freeswitch_inbound.go
+++ gofaxserver/freeswitch_inbound.go
@@ -246,7 +246,7 @@ func (e *EventSocketServer) handler(c *eventsocket.Connection) {
 			exec("set", fmt.Sprintf("fax_enable_t38=%t", enableT38), true)
 
 			exportStr := fmt.Sprintf("{%s}",
-				fmt.Sprintf("sip_execute_on_image='t38_gateway %s'", "self nocng"),
+				fmt.Sprintf("sip_execute_on_image='t38_gateway %s'", "self"),
 			)
 
 			/*exec("set", "absolute_codec_string=PCMU", true)
@@ -279,7 +279,7 @@ func (e *EventSocketServer) handler(c *eventsocket.Connection) {
 			logf(logrus.InfoLevel, "FS_INBOUND → INBOUND BRIDGE gateway=%s", map[string]interface{}{"uuid": channelUUID.String()}, bridgeGw)
 			exec("set", fmt.Sprintf("fax_enable_t38=%t", enableT38), true)
 			// exec("set", fmt.Sprintf("fax_enable_t38_request=%t", requestT38), true)
-			exec("set", fmt.Sprintf("sip_execute_on_image=t38_gateway %s", "peer nocng"), true)
+			exec("set", fmt.Sprintf("sip_execute_on_image=t38_gateway %s", "peer"), true)
 			bridgeStart = time.Now()
 			exec("bridge", fmt.Sprintf("sofia/gateway/%s/%s", bridgeGw, dstNum), true)
 		}
